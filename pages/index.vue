f18225200c2b5a599e9bfabd21f17a77539b6df1
Laurens Verspeek <l.verspeek@gmail.com>
2021-05-07 17:37:54 +0200

    add loader styling

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,14 @@
   <section class="section">
     <div class="container">
       <div class="has-text-centered">
-        <h4 class="title">Welcome to the Effect Bridge</h4>
+        <h2 class="title">Welcome to the Effect Bridge</h2>
         <div v-if="!wallet">
           <a class="button is-secondary" @click="$wallet.loginModal = true">
             <strong>Connect EOS Wallet</strong>
           </a>
         </div>
         <div v-else>
-          <span>{{ wallet.auth.accountName }}</span>
+          <h4 class="subtitle">{{ wallet.auth.accountName }}</h4>
         </div>
       </div>
     </div>
