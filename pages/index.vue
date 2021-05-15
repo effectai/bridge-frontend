27b81be92abb660ffd579915ee91759cc07ad6b6
David Britt <david@effect.ai>
2021-05-15 10:21:36 +0200

    Add chain functionality

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,8 @@
                 <br>
                 <div v-if="!wallet">
                     <a class="button is-secondary" @click="$wallet.loginModal = true">
-                                            <strong>Connect EOS 🖖</strong>
-                                          </a>
+                                                                                        <strong>Connect EOS 🖖</strong>
+                                                                                      </a>
                 </div>
                 <div v-else>
                     <h4 class="subtitle">{{ wallet.auth.accountName }}</h4>
@@ -19,60 +19,76 @@
                 <br>
                 <div v-if="isAccountConnected">
                     <a class="button is-secondary" :disabled="currentProvider != null">
-                                            <strong>Metamask Connected 🦊</strong>
-                                          </a>
+                                                                                        <strong>Metamask Connected 🦊</strong>
+                                                                                      </a>
                 </div>
                 <div v-else-if="!this.isMetaMaskInstalled">
                     <a class="button is-secondary" @click="this.onMetaMaskConnect">
-                                            <strong>Connect Metamask 🦊</strong>
-                                          </a>
+                                                                                        <strong>Connect Metamask 🦊</strong>
+                                                                                      </a>
                 </div>
                 <div v-else>
                     <a class="button is-secondary" href="https://metamask.io/download.html" target="_blank">
-                                            <strong>Install MetaMask 🦊</strong>
-                                          </a>
+                                                                                        <strong>Install MetaMask 🦊</strong>
+                                                                                      </a>
                 </div>
 
                 <!-- Binance -->
                 <br>
                 <div v-if="isAccountConnected">
                     <a class="button is-secondary" :disabled="currentProvider != null">
-                                            <strong>Binance Connected🔶</strong>
-                                          </a>
+                                                                                        <strong>Binance Connected🔶</strong>
+                                                                                      </a>
                 </div>
                 <div v-else-if="isBinanceInstalled">
                     <a class="button is-secondary" @click="this.onBinanceConnect">
-                                            <strong>Connect BSC Wallet 🔶</strong>
-                                          </a>
+                                                                                        <strong>Connect BSC Wallet 🔶</strong>
+                                                                                      </a>
                 </div>
                 <div v-else>
                     <a class="button is-secondary" href="https://docs.binance.org/smart-chain/wallet/binance.html" target="_blank">
-                                                    <strong>Install BSC Wallet 🔶</strong>
-                                                  </a>
+                                                                                                <strong>Install BSC Wallet 🔶</strong>
+                                                                                              </a>
                 </div>
 
-                <!-- WalletConnect -->
+                <!-- WalletConnect Web3 -->
                 <br>
                 <div>
-                    <a class="button is-secondary" @click="this.onWalletConnect" :disabled="currentProvider != null">
-                                                    <strong>WalletConnect 📱</strong>
-                                                  </a>
+                    <a class="button is-secondary" @click="this.onWalletConnectWeb3" :disabled="currentProvider != null">
+                                                                                                <strong>WalletConnect Web3 📱</strong>
+                                                                                              </a>
+                </div>
+
+                <!-- Change Chain -->
+                <br>
+                <div>
+                    <a class="button is-secondary" @click="this.handleChainChange" :disabled="false">
+                                                                                                <strong>Change Chain ⛓️</strong>
+                                                                                              </a>
+                </div>
+
+                <!-- Change Chain -->
+                <br>
+                <div>
+                    <a class="button is-secondary" @click="this.addChain" :disabled="false">
+                                                                                                <strong>Add Chain 🔌</strong>
+                                                                                              </a>
                 </div>
 
                 <!-- Disconnect // Figure out how we can do this.  -->
                 <br>
                 <div>
-                    <a class="button is-danger" :disabled="this.walletConnected || this.currentProvider == null">
-                                            <strong>Disconnect 🚧</strong>
-                                          </a>
+                    <a class="button is-warning" :disabled="this.walletConnected || this.currentProvider == null">
+                                                                                        <strong>Disconnect 🚧</strong>
+                                                                                      </a>
                 </div>
 
                 <!-- Educational Resources -->
                 <br>
                 <div>
                     <a class="button is-warning" href="https://docs.pancakeswap.finance/get-started/connection-guide" target="_blank">
-                                            <strong>Learn how to connect 📞</strong>
-                                          </a>
+                                                                                        <strong>Learn how to connect 📞</strong>
+                                                                                      </a>
                 </div>
 
                 <br>
@@ -86,31 +102,24 @@
 </template>
 
 <script>
-//web3 provider
+/**
+ * When using the walletconnect protocol we need to make sure to use the custom requests.
+ * https://docs.binance.org/walletconnect.html
+ */
 import WalletConnectProvider from "@walletconnect/web3-provider";
 
-//StandaloneClient
-import WalletConnect from "@walletconnect/client";
-import QRCodeModal from "@walletconnect/qrcode-modal";
-
-
 const walletProvider = new WalletConnectProvider({
+    chainId: 1, // For some reason chainID needs to be 1.
     rpc: {
-        97: "https://data-seed-prebsc-1-s1.binance.org:8545/", //testnet
-        56: "https://bsc-dataseed.binance.org/" //mainnet
+        1: "https://data-seed-prebsc-1-s3.binance.org:8545/"
     },
+    bridge: "https://bridge.walletconnect.org", // redundant?
+    qrcode: true, // redundant?
+    // qrcodeModal: QRCodeModal, //redundant?
     qrcodeModalOptions: {
-        mobileLinks: [
-            "metamask",
-            "trust",
-            "rainbow",
-            "argent",
-            "imtoken",
-            "pillar"
-        ]
+        mobileLinks: ["metamask", "trust", "rainbow", "argent"]
     }
 })
-
 
 export default {
     data() {
@@ -120,8 +129,14 @@ export default {
             walletConnect: walletProvider || null, //connect to mobile wallet; trust, metamask, ...
             web3Provider: null,
             currentAccount: [],
-            walletConnected: null, //Figure out how to update this dynamically. Probably during `mounted`
-            currentProvider: null
+            walletConnected: null, // Does it make sense to do this at the beginning?
+            currentProvider: null,
+            bscTestnetId: 97,
+            bscTestnetHexId: '0x61',
+            bscTestnetRPC: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+            bscMainnetID: 56,
+            bascMainnetHexId: '0x38',
+            bscMainnetRPC: 'https://bsc-dataseed.binance.org/'
         }
     },
 
@@ -140,29 +155,20 @@ export default {
         },
 
         isAccountConnected() {
-            // const web3Provider
-            // walletProvider.
-            // return this.metamask.listAccounts()
-        },
-
-        // currentAccount() {
-        //     return this.currentAccount[0];
-        // },
-
-        // currentProvider() {
-        //     return this.currentProvider
-        // }
-
+            return this.currentAccount > 0
+        }
     },
 
     methods: {
         async onMetaMaskConnect() {
             try {
                 console.log('Connecting MetaMask')
-                this.currentProvider = this.metamask
+                this.registerProviderListener(this.metamask)
+                this.addChain()
                 this.currentAccount = await this.currentProvider.request({
                     method: 'eth_requestAccounts'
                 })
+                this.registerProviderListener()
             } catch (mmError) {
                 console.error(mmError)
             }
@@ -170,54 +176,42 @@ export default {
 
         async onBinanceConnect() {
             try {
-              console.log('Connecting Binance')
-              this.currentProvider = this.binance
-              this.currentAccount = await this.binance.request({
-                method: 'eth_requestAccounts'
-              })
+                console.log('Connecting Binance')
+                this.registerProviderListener(this.binance)
+                this.addChain()
+                this.currentAccount = await this.currentProvider.request({
+                    method: 'eth_requestAccounts'
+                })
+                this.registerProviderListener()
             } catch (bscError) {
-              console.error(bscError)
+                console.error(bscError)
             }
         },
 
-        // Web3provider
-        // async onWalletConnect() {
-        //     //  Create WalletConnect Provider this is for the qr code mdal pop up at the moment.
-        //     this.currentProvider = walletProvider
-        //     await this.currentProvider.enable()
-        //     //Integrate dapp with the provider
-        //     // const web3 = new Web3(walletConnectProvider)
-        // },
+        async onWalletConnectWeb3() {
 
-        // How does WalletConnect know that it needs to be on the binance chain?
-        async onWalletConnect() {
-            // Create a connector
-            const connector = new WalletConnect({
-                bridge: "https://bridge.walletconnect.org", // Required
-                qrcodeModal: QRCodeModal,
-                qrcodeModalOptions: {
-                    mobileLinks: [
-                        "metamask",
-                        "trust",
-                        "rainbow",
-                        "argent"
-                    ]
-                }
-            });
-            if(!connector.connected){
-              connector.createSession()
-              console.log(`
-                ChainID: ${connector.chainId}
-                networkID: ${connector.networkId}
-              `)
+            try {
+                this.registerProviderListener(walletProvider)
+                this.addChain()
+
+                // Launches QR-Code Modal
+                await walletProvider.enable()
+
+                //Integrate dapp with the provider
+                // const web3 = new Web3(walletConnectProvider)
+
+            } catch (walletConnectError) {
+                console.error(walletConnectError)
             }
         },
 
+
+        // Does this still work?
         async walletDisconnect() {
             await this.currentProvider.disconnect();
         },
 
-        //Does not properly work at the moment.
+        //Does not properly work at the moment, does not work at the moment - CONCEPT
         async disconnectAccount() {
             console.log('Disconnecting')
 
@@ -229,6 +223,9 @@ export default {
             } catch (disconnectError) {
                 console.error('Error disconnecting from metaMask', disconnectError)
             }
+
+            // This method is accesible on the WalletConnectProvider interface.
+            // await this.currentProvider.disconnect();
         },
 
         // Handle when the user changes their account number - DOES NOT WORK AT THE MOMENT; CONCEPT
@@ -244,25 +241,66 @@ export default {
 
         // Hanlde chain (network) and chainchanged - DOES NOT WORK AT THE MOMENT; CONCEPT
         async handleChainChange() {
-            const chainId = await ethereum.request({ method: 'eth_chainId' });
-            handleChainChanged(chainId);
+            const chainId = await this.currentProvider.request({ method: 'eth_chainId' });
+            console.log(`
+              chainID: ${chainId}
+            `)
 
-            ethereum.on('chainChanged', handleChainChanged);
+            if (chainId != this.bscTestnetHexId) {
+                handleChainChanged(chainId);
 
-            function handleChainChanged(_chainId) {
-                // We recommend reloading the page, unless you must do otherwise
-                window.location.reload();
+                this.currentProvider.on('chainChanged', handleChainChanged);
+
+                function handleChainChanged(_chainId) {
+                    // It  is recomended to reload the page.
+                    window.location.reload();
+                }
             }
+
         },
 
-        // async isConnected
+        async addChain() {
 
-    },
+            const chainId = await this.currentProvider.request({ method: 'eth_chainId' });
+            if (chainId != this.bscTestnetHexId) {
 
-    created() {
+                try {
+                    const chainObject = {
+                        chainId: this.bscTestnetHexId,
+                        chainName: "Binance Smart Chain Testnet",
+                        nativeCurrency: {
+                            name: "Binance Chain Token",
+                            symbol: 'BNB',
+                            decimals: 18
+                        },
+                        rpcUrls: ['https://data-seed-prebsc-1-s3.binance.org:8545/'],
+                        blockExplorerUrls: ['https://testnet.bscscan.com']
+                    }
 
-        if (this.currentProvider) {
+                    const chainId = await this.currentProvider.request({
+                        method: 'wallet_addEthereumChain',
+                        params: [chainObject]
+                    })
 
+                    if (chainId = !null) throw Error(`AddChainError: ${chainId}`)
+                } catch (addChainError) {
+                    console.error(addChainError)
+                }
+            }
+
+        },
+
+        // Register the provider with Web3
+        async registerWeb3(provider) {
+            console.debug(`web3 here.`)
+            const web3 = new Web3(provider)
+        },
+
+        // Should we use `this.provider` or pass provider as a parameter?
+        // Actually the I am already reg
+        async registerProviderListener(provider) {
+            this.currentProvider = provider
+            console.debug(`provider.on() events here.`)
             this.currentProvider.on('connect', () => {
                 console.log('Connecting')
                 this.walletConnected = true
@@ -279,13 +317,14 @@ export default {
                 this.currentAccount = newAccount
             })
 
-            // inform user that they are not on the right chain
+            // inform if the chain is changing
             this.currentProvider.on('chainChanged', console.log)
 
-            // Inform user that they are not on the right network.
-            this.currentProvider.on('networkChanged', console.log)
-
         }
+
+    },
+
+    created() {
 
     },
 }
