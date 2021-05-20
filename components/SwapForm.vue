d6c1ab0ead2c7e092b00b69b510017657f202703
Laurens Verspeek <l.verspeek@gmail.com>
2021-05-20 12:13:38 +0200

    swap form

--- components/SwapForm.vue
+++ components/SwapForm.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <div class="box is-horizontal-centered px-6" style="max-width: 550px">
-      <div class="columns is-vcentered">
-        <div class="column is-align-self-stretch is-5">
-          <small class="is-size-7">From</small>
+      <div class="columns is-vcentered" :class="{'is-flex-direction-row-reverse': !swapFromEOS}">
+        <div class="column is-align-self-stretch is-5 ">
+          <small class="is-size-7"><span v-if="swapFromEOS">From</span><span v-else>To</span></small>
           <div class="box is-shadowless has-border has-text-centered is-fullheight">
             <div class="subtitle has-text-weight-semibold mb-2">EOS</div>
             <img src="~assets/img/EOS-logo.svg" height="100"/>
@@ -22,13 +22,13 @@
           </div>
         </div>
         <div class="column is-2 has-text-centered">
-          <a class="has-text-centered" @click="swapFromEOS = !swapFromEOS">
+          <a class="has-text-centered" @click="switchChains">
             <i class="fas fa-exchange-alt"></i><br>
             <small class="is-size-7"><a @click="">switch</a></small>
           </a>
         </div>
         <div class="column is-align-self-stretch is-5">
-          <small class="is-size-7">To</small>
+          <small class="is-size-7"><span v-if="swapFromEOS">To</span><span v-else>From</span></small>
           <div class="box is-shadowless has-border has-text-centered">
             <div class="subtitle has-text-weight-semibold mb-2">BSC</div>
             <img src="~assets/img/BSC-logo.svg" height="100"/>
@@ -47,8 +47,46 @@
           </div>
         </div>
       </div>
+      <div class="is-size-7 columns mb-0 is-mobile">
+        <div class="column py-0">
+          Amount
+        </div>
+        <div class="column has-text-right py-0">
+          Balance:
+          <span v-if="swapFromEOS">
+          <span v-if="$eos.efxAvailable !== null"><a @click="efxAmount = $eos.efxAvailable">{{ $eos.efxAvailable }}</a></span>
+          <span v-else>-</span>
+        </span>
+          <span v-else>
+          <span v-if="$bsc.efxAvailable !== null"><a @click="efxAmount = $bsc.efxAvailable">{{
+              bsc.efxAvailable
+            }}</a></span>
+          <span v-else>-</span>
+        </span>
+        </div>
+      </div>
+      <div class="field has-addons">
+        <div class="control is-flex-grow-1">
+          <input class="input is-medium" type="number" placeholder="Amount to swap"
+                 min="0" v-model="efxAmount">
+        </div>
+        <p class="control">
+          <a class="button is-static is-medium">
+            <span v-if="!swapFromEOS">p</span>EFX
+          </a>
+        </p>
+      </div>
+      <div class="is-size-7">
+        To your <span v-if="swapFromEOS">BSC Address</span><span v-else>EOS Account</span>
+      </div>
+      <div class="field">
+        <input class="input" disabled :value="swapFromEOS ? (bscWallet ? bscWallet[0] : '- login with your BSC wallet -') : (eosWallet ? eosWallet.auth.accountName : '- login with your EOS wallet -')" type="text" />
+      </div>
+      <button :disabled="!efxAmount || !eosWallet || !bscWallet" class="button is-medium is-accent is-fullwidth mt-5"
+              @click="onSwap">
+        <strong>Swap</strong>
+      </button>
     </div>
-
     <div class="columns is-centered mt-4">
       <div class="column is-5">
         <!-- Progress bar -->
@@ -57,58 +95,16 @@
           <div class="notification is-danger" v-if="this.swapError">{{ this.swapError }}</div>
           <progress v-if="!this.swapError" class="progress is-primary" :value="this.progress" max="100"></progress>
         </div>
-
-        <!-- Forms -->
-        <div class="eos-bsc-form" v-if="swapFromEOS">
-          <h3 class="title is-3">EOS -> BSC</h3>
-          <div class="field has-addons">
-            <div class="control is-flex-grow-1">
-              <input class="input is-medium" type="number" placeholder="Amount to swap"
-                     onkeyup="if(this.value<0){this.value= this.value * -1}" v-model="efxAmount">
-            </div>
-            <p class="control">
-              <a class="button is-static is-medium">
-                EFX
-              </a>
-            </p>
-          </div>
-          <span class="icon" @click="swapFromEOS = !swapFromEOS">
-          <i class="fas fa-exchange-alt"></i>
-        </span>
-        </div>
-
-        <div class="bsc-eos-form" v-if="!swapFromEOS">
-          <h3 class="title is-3">BSC -> EOS</h3>
-          <div class="field has-addons ">
-            <div class="control is-flex-grow-1">
-              <input class="input is-medium" type="number" placeholder="Amount to swap"
-                     onkeyup="if(this.value<0){this.value= this.value * -1}" v-model="pefxAmount">
-            </div>
-            <p class="control">
-              <a class="button is-static is-medium">
-                pEFX
-              </a>
-            </p>
-          </div>
-          <span class="icon" @click="swapFromEOS = !swapFromEOS">
-          <i class="fas fa-exchange-alt"></i>
-        </span>
-        </div>
-        <button :disabled="disabled || (!efxAmount && !pefxAmount)" class="button is-medium is-danger is-fullwidth mt-5"
-                @click="onSwap()">
-          <strong>Swap</strong>
-        </button>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  data() {
+  data () {
     return {
       swapFromEOS: true,
       efxAmount: null,
-      pefxAmount: null,
       inProgress: false,
       progress: null,
       progressText: null,
@@ -117,14 +113,14 @@ export default {
   },
   computed: {
     // EOS
-    eosWallet() {
+    eosWallet () {
       return (this.$eos) ? this.$eos.wallet : null
     },
-    bscWallet() {
+    bscWallet () {
       return (this.$bsc) ? this.$bsc.wallet : null
     }
   },
-  mounted() {
+  mounted () {
     this.$nuxt.$on('progressUpdate', (update) => {
       this.inProgress = update.inProgress;
       this.progress = update.progress;
@@ -133,13 +129,17 @@ export default {
     });
   },
   methods: {
-    async onSwap() {
+    switchChains () {
+      this.swapFromEOS = !this.swapFromEOS
+      this.efxAmount = null
+    },
+    async onSwap () {
       console.log('Start swap...');
       this.$ptokens.init(this.$bsc.currentProvider, this.$bsc.currentAccount)
       if (this.swapFromEOS) {
         this.$ptokens.swapToBsc(this.efxAmount)
       } else {
-        this.$ptokens.swapToEos(this.pefxAmount)
+        this.$ptokens.swapToEos(this.efxAmount)
       }
     },
   }
