0aba986052a0268ccee4e39abafc888266645cbf
David Britt <david@effect.ai>
2021-08-10 15:37:21 +0200

    Add contract abi's and contract.js

---
+++ plugins/contracts.js
@@ -0,0 +1,6 @@
+import Web3 from "web3";
+
+
+// Maximum allowable value for a uint256
+const MaxInt256 = (Web3.utils.BN.from("0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+
