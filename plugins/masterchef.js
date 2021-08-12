b3a1b835b7b5527588cb116dcc559ac555e0bf41
David Britt <david@effect.ai>
2021-08-12 12:53:20 +0200

    Add imports

--- plugins/masterchef.js
+++ plugins/masterchef.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Web3 from 'web3'
+import { BN } from "web3-utils";
+import { abi as PancakePair } from "@/static/abi/PancakePair.json"
+import { abi as BEP20 } from "@/static/abi/BEP20.json"
+import { abi as MasterChef } from "@/static/abi/MasterChef.json"
+
 
 // Dus mijn plan nu is om alles hier in te schrijven en het hier te gebruiken.
 
