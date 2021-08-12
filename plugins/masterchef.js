e10baf4de5b513e1c0604966614cfa5bc15e4b36
David Britt <david@effect.ai>
2021-08-12 12:48:30 +0200

    Add masterchef plugin template

---
+++ plugins/masterchef.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Web3 from 'web3'
+
+// Dus mijn plan nu is om alles hier in te schrijven en het hier te gebruiken.
+
+export default (context, inject) => {
+
+  const masterchef = new Vue({
+    data() {
+    },
+    computed: {
+    },
+    methods: {
+    },
+    created() {
+    },
+    mounted() {
+    },
+
+  })
+
+  inject('masterchef', masterchef)
+
+}
